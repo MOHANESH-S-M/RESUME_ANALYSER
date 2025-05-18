73bcd9cbfb537594c132db1da24d78553ae31b14
MOHANESH <smmohanesh3@gmail.com>
2025-05-18 10:22:17 +0530

    done

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask,render_template,request
-
+from flask import Flask,render_template,request , redirect ,url_for,session
+from resume_parser import extract_text_from_docx,extract_skills
 import os
 
 app = Flask(__name__)
@@ -14,12 +14,15 @@ def index():
         resume = request.files['resume']
         os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
         resume_path = os.path.join(app.config['UPLOAD_FOLDER'],resume.filename)
-        
-
         resume.save(resume_path)
 
-        return "Resume Uploaded Successfully"
+        text = extract_text_from_docx(resume_path)
+        skills = extract_skills(text)
+        return render_template("results.html", text=text, skills=skills)
+        
     return render_template('index.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- resume_parser.py
+++ resume_parser.py
@@ -1,17 +1,32 @@
 from docx import  Document
 
 COMMON_SKILLS = [
-    'python' , 'java' , 'django' , 'sql'
+    'python' , 'java' , 'django' , 'sql',"Traine" ,"internship","systems"
 ]
 
 def extract_text_from_docx(file_path):
     doc = Document(file_path)
-    full_text = ''
-    for para in doc:
-        full_text += para.txt + '\n'
-    return full_text.lower()
+    full_text = []
+
+    # Extract paragraphs
+    for para in doc.paragraphs:
+        if para.text.strip():
+            full_text.append(para.text.strip())
+
+    # Extract text from tables
+    for table in doc.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                cell_text = cell.text.strip()
+                if cell_text:  # Avoid empty cells
+                    full_text.append(cell_text.lower())
+
+    return '\n'.join(full_text)
+
 def extract_skills(text):
     found_skills = []
-    if skill.lower() in text:
-        found_skills.append(skill)
+    for skill in COMMON_SKILLS:
+        if skill.lower() in text:
+            found_skills.append(skill)
     return found_skills
+   
